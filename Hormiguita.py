f66b2dbd2e04ff41178822dc88af17505328e042
Suicide has been <Sguerreroa@uninorte.edu.co>
2020-03-24 16:24:18 -0500

    Se añadio GUI para establecer la pos inicial

--- Hormiguita.py
+++ Hormiguita.py
@@ -1,16 +1,45 @@
+from tkinter import *
 import tkinter as tk
 import turtle
 import numpy as np
 import time
 
+root = Tk()
+def speichern():
+  global x0,y0
+  x0 = int(ex.get())
+  y0 = int(ey.get())
+  root.destroy()
+root.title("Datos iniciales")
+mylabel = Label(root,text="Ingrese las posicion inicial de la hormiga:")
+ex = Entry(root, width=15)
+ex.insert(0, "X")
+ey = Entry(root, width=15)
+ey.insert(0, "Y")
+mybutton = Button(root,text="Guardar e iniciar",command=speichern)
+mylabel.grid(row=0,column=0)
+ex.grid(row=1,column=0)
+ey.grid(row=1,column=1)
+mybutton.grid(row=2,column=0, columnspan=2,) 
+root.mainloop()
+
+
+print(x0)
+print(y0)
+
+
+def einrichten():
+    global x0, y0,zelleW,zelleH,pxm,pym, MATRIX
+    zelleW = 0
+    zelleH = 0
+    MATRIX = np.zeros((100, 100))
+    pxm = 0
+    pym = 0
+
+
 #Lassen Sie uns die Ausgangsposition einstellen
-x0 = 600/2
-y0 = 600/2
-zelleW = 0
-zelleH = 0
-MATRIX = np.zeros((100, 100))
-pxm = 0
-pym = 0
+
+
 #Richtungen 
 #1 = rechts - 2 = süd - 3 = links - 4 = nord
 #Richtung einstellen
@@ -38,6 +67,7 @@ def ausgangsposition(papier):
         pxm -= 1
     zeile, säule = MATRIX.shape
     papier.create_rectangle( pxm*zelleW, pym*zelleH, pxm*zelleW+zelleW, pym*zelleH+zelleH, fill='black')
+
 
 
 def einrichten_gitter(papier):
@@ -109,6 +139,7 @@ def aktualiserenPapier(papier):
         return True
 
 def init():
+    einrichten()
     fenster = tk.Tk(className=" Hormiguita Langton")
     papier = tk.Canvas(fenster, width=600, height=600)
     fenster.resizable(False, False)
